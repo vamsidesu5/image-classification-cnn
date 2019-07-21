db76eeb221971be7204f97cbb568048b7ee27f61
vamsidesu5 <vamsidesu5@gmail.com>
2019-07-21 19:06:21 -0400

    adding classifier

---
+++ cnn.py
@@ -0,0 +1,7 @@
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+classifier = Sequential()
